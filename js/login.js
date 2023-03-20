e100d85571d49c3f70ce1b38c2f5db1c9528f1ec
RaymondLam18 <1024322@hr.nl>
2023-03-20 13:09:15 +0100

    Pagina's

---
+++ js/login.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', init);
+
+let loginForm
+let userField
+let passwordField
+let loginSubmit
+let username
+let password
+
+function init () {
+
+    loginForm = document.getElementById("login-form")
+    userField = document.getElementById("gebruikers-field")
+    passwordField = document.getElementById("wachtwoord-field")
+    loginSubmit = document.getElementById("login-submit")
+
+    loginSubmit.addEventListener("click", formSubmitHandler)
+}
+
+function formSubmitHandler (event) {
+    event.preventDefault()
+
+    username = loginForm.gebruikersnaam.value
+    password = loginForm.wachtwoord.value
+
+}
